111fc82a849b512742253bb8e9b764b176d5d3b8
arthurfreitasys <arthur.freitasys@gmail.com>
2025-06-04 20:40:52 -0300

    Delete sanquim login directory

--- sanquim login/conecta.php
+++
@@ -1,5 +0,0 @@
-<?php
-$link=mysqli_connect("localhost","root","")or die("Não achei o mysql") 
-or die("não achei o mysql");
-mysqli_select_db($link, "sanquimt") or die("não achei o banco de dados");
-?>
